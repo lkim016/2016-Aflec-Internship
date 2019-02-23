eb88c1d1216874c14d6a82b6ac57e46c1afd91f8
Lori Kim <kim.lori57@gmail.com>
2019-02-23 02:47:24 -0500

    Add files via upload

---
+++ Everwell Wordpress/footer-everwell.php
@@ -0,0 +1,50 @@
+<footer>
+	<div class = "footerContain">
+		<table>
+			<tr>
+				<th class = "column1">EVERWELL PASADENA</th>
+				<th class = "column2">CONTACT YOUR AGENT</th>
+				<th class = "column3">FOLLOW</th>
+			</tr>
+			<tr>
+				<td class = "column1">
+					<p>1 S FAIR OAKS AVE.</br>SUITE #200,</br>PASADENA, CA 91105</p>
+					<p>PHONE: (626) 796-7077</br>
+					FAX: (626) 796-7060</p>
+				</td>
+				<td class = "column2">
+					<form action>
+						<p> <input name = "email" type = "text" placeholder = "E-mail address" /> </p>
+						<p> <input name = "subject" type = "text" placeholder = "Subject" /> </p>
+						<p> <textarea name = "message" type = "text" /></textarea> </p>
+						<p> <input name = "btnsubmit" type = "submit" />
+					</form>
+				</td>
+				<td class = "column3">
+					<div>
+            <a href="https://www.facebook.com/Everwell-Pasadena-1548917178752690/?ref=hl" class="facebook_img"><img name="fbImage" src="http://www.everwellpasadenabeta.com/wp-content/uploads/2016/03/contact-us-fb.png"></a>
+					</div>
+					<div>
+            <a href="https://twitter.com/EverwelPasadena/" class="twitter_img"><img name="twitImage" src="http://www.everwellpasadenabeta.com/wp-content/uploads/2016/03/contact-us-twitter.png"></a>
+					</div>
+					<div>
+            <a href="https://www.instagram.com/everwellpasadena/" class="instagram_img"><img name="instaImage" src="http://www.everwellpasadenabeta.com/wp-content/uploads/2016/03/contact-us-instagram.png"></a>
+					</div>
+				</td>
+			</tr>
+				<!--- ONLY VISIBLE ON MOBILE -->
+			<tr>
+				<td class = "mobCol">
+					<div>
+            <a href="https://www.facebook.com/Everwell-Pasadena-1548917178752690/?ref=hl" class="facebook_img"><img align = "left" name="fbImage" src="http://www.everwellpasadenabeta.com/wp-content/uploads/2016/03/contact-us-fb.png"></a>
+            <a href="https://twitter.com/EverwelPasadena/" class="twitter_img"><img name="twitImage" align = "middle" src="http://www.everwellpasadenabeta.com/wp-content/uploads/2016/03/contact-us-twitter.png"></a>
+            <a href="https://www.instagram.com/everwellpasadena/" class="instagram_img"><img name="instaImage" align = "right" src="http://www.everwellpasadenabeta.com/wp-content/uploads/2016/03/contact-us-instagram.png"></a>
+          </div>
+				</td>
+			</tr>
+		</table>
+	</div>
+</footer>
+
+
+</html>
---
+++ Everwell Wordpress/index.php
@@ -0,0 +1,183 @@
+<?php /* Template Name: Everwell Page */ ?>
+<?php get_header('everwell'); ?>
+
+    <!---BODY-->
+<body>
+      <div id="popup" class="popup_content">
+        <p class="popup_Title"><b>Policy Title</b></p>
+        <p id="popuptext" class="popup_Text">
+        &emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><br>
+        <ul>
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet</li>
+        </ul>
+     <a id="popupClose" class = "popup_Close" href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
+
+    <div id="fade" class="black_overlay"></div>  
+          <div class="service-text" id="service-text">
+
+            <div class="service-container">
+
+           	  <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                	<div class="service-item">
+  							  <div class="service-thumb">
+  								<div class="overlay-p">
+  								</div>
+  						  	</div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+  						  </div>
+             </a>
+              </div>
+              <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+             <div class="job-column-1 job-column-2 job-column-3">
+             <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+            <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+
+             <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+            <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+            <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+             <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+              <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+              <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+             <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+             <div class="job-column-1 job-column-2 job-column-3">
+              <a href = "javascript:void(0)" onclick = "document.getElementById('popup').style.display='block';document.getElementById('fade').style.display='block'">
+                  <div class="service-item">
+                  <div class="service-thumb">
+                  <div class="overlay-p">
+                  </div>
+                  </div> <!-- /.service-thumb -->
+                  <p><b>SHORT-TERM POLICY</b><br>
+                  <font class="learnFont" size="2">Learn More ></font></p>
+                </div>
+             </a>
+              </div>
+
+
+    </div>
+</div>
+<div class="register_div">
+  <a href="https://www.everwellbenefits.com" class="registerButton">REGISTER FOR ENROLLMENT</a>
+
+</div>
+
+
+</body>
+
+<!-- END OF BODY -->
+
+<?php get_footer('everwell'); ?>
